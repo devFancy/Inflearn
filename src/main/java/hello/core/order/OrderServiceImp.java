553f4ed14ba891021a81a0ff885decd53261d189
junyong <jymoon1121@gmail.com>
2023-01-16 21:56:37 +0900

    feat : 주문과 할인 도메인 개발

---
+++ src/main/java/hello/core/order/OrderServiceImp.java
@@ -0,0 +1,21 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImp implements OrderService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId); // 회원정보 조회
+        int discountPrice = discountPolicy.discount(member, itemPrice); // 할인정책에 넘겨서 할인된 가격을 받음
+
+        return new Order(memberId, itemName, itemPrice, discountPrice); // 최종 생성된 주문을 반환
+    }
+}
