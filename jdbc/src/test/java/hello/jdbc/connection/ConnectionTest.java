d1c2d00f3bea71c55fdab5cbae42048d94ad3e78
devfancy <fancy.junyongmoon@gmail.com>
2024-01-15 13:00:35 +0900

    feat: [Spring DB 1편] DataSource 예제2 - 커넥션 풀

--- jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
@@ -11,6 +12,7 @@ import static hello.jdbc.connection.ConnectionConst.*;
 
 @Slf4j
 public class ConnectionTest {
+    public static final int DEFAULT_SIZE = 10;
     @Test
     void driverManager() throws SQLException {
         Connection con1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
@@ -18,7 +20,21 @@ public class ConnectionTest {
         log.info("connection={}, class={}", con1, con1.getClass());
         log.info("connection={}, class={}", con2, con2.getClass());
     }
-    
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        // 커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource(); // 스프링에서 jdbc를 사용하면 자동으로 import 됨
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(DEFAULT_SIZE); // 기본 사이즈 10
+        dataSource.setPoolName("MyPool");
+
+        useDateSource(dataSource);
+        Thread.sleep(1000); // 1초
+    }
+
     @Test
     void dataSourceDriverManager() throws SQLException {
         // DriverMangerDataSource - 항상 새로운 커넥션을 획득
